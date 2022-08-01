8c9f83222776ef1ac69050523c065e6676d32157
LeeYooBin <santosroberdan25@gmail.com>
2022-08-01 10:53:00 -0300

    commit

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 function main(){
-    const cep = document.querySelector('.cep');
+    const cep = document.querySelector('.cep').value;
     const rua = document.querySelector('.rua');
     const bairro = document.querySelector('.bairro');
     const cidade = document.querySelector('.cidade');
     const estado = document.querySelector('.estado');
     const submit = document.querySelector('.submit');
 
-    submit.addEventListener('click', async () => {
+    submit.addEventListener('click', async (e) => {
+        e.preventDefault();
         const search = cep.replace('-', '');
-        if(search.length !== 8) alert('CEP inválido!');
+        if(search.length !== 8) alert('CEP inválido');
         else{
-            const response = await fetch(`http://viacep.com.br/ws/${search}/json/`);
-            const data = await response.json();
-            rua.value = data.logradouro;
-            bairro.value = data.bairro;
-            cidade.value = data.cidade;
-            estado.value = data.estado;
+            const url = `http://viacep.com.br/ws/${search}/json/`;
+            const options = {
+                method: 'GET',
+                mode: 'no-cors',
+                cache: 'default'
+            }
+            try{
+                const response = await fetch(url, options);
+                const data = await response.json();
+                rua.value = data.logradouro;
+                bairro.value = data.bairro;
+                cidade.value = data.localidade;
+                estado.value = data.uf;
+            }
+            catch(e){
+                alert(e);
+            }
         }
-    }
-    );
+    });
 }
 main();
